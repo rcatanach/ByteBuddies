ff232c704c2081709da99c5c600062aa5ec513ce
dustinbachman <dustinbachman@hotmail.com>
2023-12-06 14:33:49 -0700

    Added remove functionality
    Also added loader to upload images page, and changed some small layout stuff in wardrobe.

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth
 import { signOut } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js';
 //Firestore is used to store user inventory data (wardrobe)
 //May need to use realtime database for web instead
-import { getFirestore, doc, setDoc, getDoc, getDocs, collection, query, where } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js';
+import { getFirestore, doc, setDoc, getDoc, getDocs, collection, query, where, deleteDoc } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js';
 
 import { getStorage, ref, uploadBytes, getDownloadURL} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-storage.js'
 
@@ -47,4 +47,4 @@ export {uploadBytes, ref, getDownloadURL}; // Export the uploadBytes function an
 
 //firestore functions
 export const db = getFirestore(app);
-export {doc, setDoc, getDoc, getDocs, collection, query, where}
+export {doc, setDoc, getDoc, getDocs, collection, query, where, deleteDoc}
